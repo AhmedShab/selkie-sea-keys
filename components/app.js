49a4002bf75a2db945b6f6aef0cd06497cf0d429
Ahmed Shaaban & Martin <pair+AhmedShab.martybutts@devbootcamp.com>
2016-08-19 15:48:49 +1200

    added four more products

--- components/app.js
+++ components/app.js
@@ -34,8 +34,16 @@ class App extends Component {
                  addCart={this.addCart} price='$100'/>
         <Product name="Piano key" image='https://s-media-cache-ak0.pinimg.com/736x/f2/52/43/f25243ff44583f544720e06c2b88a7b2.jpg'
                  addCart={this.addCart} price='$5' amount={this.changeAmount}/>
-               <Product name="John key" image='http://cdn.notonthehighstreet.com/fs/5d/2e/8c6a-f446-473d-b7be-044c46147ca9/original_large-ornate-key-in-gold.jpg'
+        <Product name="John key" image='http://www.acroprintstore.com/media/products2011/key1.jpg'
                  addCart={this.addCart} price='50c' amount={this.changeAmount}/>
+        <Product name="Key to Life" image='http://cdn.notonthehighstreet.com/fs/5d/2e/8c6a-f446-473d-b7be-044c46147ca9/original_large-ornate-key-in-gold.jpg'
+                 addCart={this.addCart} price='$500' amount={this.changeAmount}/>
+        <Product name="Mon-key" image='http://cogswellgallery.com/image/cache/data/Ara%20Collection/ara_small_key_pendant-800x800.jpg'
+                addCart={this.addCart} price='$500' amount={this.changeAmount}/>
+        <Product name="Black Keys" image='http://www.saturdayeveningpost.com/wp-content/uploads/satevepost/ben-franklins-key.png'
+                addCart={this.addCart} price='$500' amount={this.changeAmount}/>
+        <Product name="Key to Treasure Chest" image='http://www.tommyjo.co.uk/content/images/thumbs/0000058_sashas-large-and-small-key-necklace_335.jpeg'
+                addCart={this.addCart} price='$500' amount={this.changeAmount}/>
       </div>
     )
   }
