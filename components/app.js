210be54605412f44a5dbe58461ab92cbd14c3d97
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-19 14:25:44 +1200

    install webpack to set up local server

--- components/app.js
+++ components/app.js
@@ -6,7 +6,7 @@ class App extends Component {
 
   constructor (props) {
     super(props)
-    this.state = {amount: [], cartProducts: []} 
+    this.state = {amount: [], cartProducts: []}
     this.addToCart = this.addToCart.bind(this)
   }
 
---
+++ components/cartList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class CartList extends Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        This is the cart page!
+      </div>
+    )
+  }
+
+}
+
+export default CartList
